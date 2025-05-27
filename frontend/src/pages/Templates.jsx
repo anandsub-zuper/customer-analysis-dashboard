a3c29a72dd66d6c58e3bc973874db745d895d569
anandsub-zuper <anandsub@zuper.co>
2025-05-27 12:33:38 -0700

    Create Templates.jsx

---
+++ frontend/src/pages/Templates.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TemplateManagement from '../components/TemplateManagement';
+
+const Templates = () => {
+  return (
+    <div className="min-h-full bg-gray-100">
+      <TemplateManagement />
+    </div>
+  );
+};
+
+export default Templates;
