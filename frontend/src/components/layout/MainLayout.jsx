4251503c425bf2615e89d2dbd59db347c5ea7f2d
anandsub-zuper <anandsub@zuper.co>
2025-05-27 12:30:08 -0700

    Update MainLayout.jsx

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../common/Navbar';
 import Sidebar from '../common/Sidebar';
 
 const MainLayout = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+  
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
-      <Sidebar />
+      <Sidebar collapsed={sidebarCollapsed} setCollapsed={setSidebarCollapsed} />
       
       {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
