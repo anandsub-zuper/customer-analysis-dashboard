9a7c4dca6cdda0fe9aed604079c080fd0d09e98e
anandsub-zuper <anandsub@zuper.co>
2025-05-26 23:49:48 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const { google } = require('googleapis');
 const { JWT } = require('google-auth-library');
 const { connect, close } = require('./services/mongoDbService');
+const configRoutes = require('./routes/configRoutes');
+
 
 // Load environment variables
 dotenv.config();
@@ -38,6 +40,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/config', configRoutes);
 
 // Connect to MongoDB on startup
 async function initializeServer() {
